203cb860f34cc2a0eafdb2d0bf82f2e8294dcba0
TeslaAngel <42201165+TeslaAngel@users.noreply.github.com>
2021-01-30 11:50:05 +0800

    Initing 2d Navmesh && 2d light

---
+++ Assets/NavMeshComponents/Scripts/NavMeshAgentMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentMover : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    public Transform target;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.updateRotation = false;
+        navMeshAgent.updateUpAxis = false;
+    }
+
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
